9a2d190fd19e42c1ee087acf1ca3321367cf88bb
kawaii <nyaa@openmailbox.org>
2016-01-11 13:58:49 -0500

    posted good end

---
+++ goodend.py
@@ -0,0 +1,27 @@
+import os
+def getVoice(x):
+    print (x);
+    try:
+        return (os.system("espeak -s 160 -v en-n '" + x + "'"));
+    except:
+        return (print("Voice is offline"));
+getVoice("neko-chan reaches the end of the cave")
+getVoice("she looks back and sees many crushed tentacles")
+getVoice("neko-chan smiles and makes some tea")
+getVoice("neko-chan waits for the sun to rise")
+getVoice("as the sun rises, she leaves the cave")
+getVoice("she sees dozens of weeaboos who failed to get inside")
+getVoice("they are all on fire as the sun burns their snow-white skin")
+getVoice("neko-chan continues on to her school to reunite with her senpais")
+getVoice("her senpais ask if she is ok")
+getVoice("neko-chan puts on sunglasses and smiles at them")
+getVoice("neko-chan continues walking to class, ignoring her senpais")
+getVoice("former senpais that is")
+getVoice("because neko-chan has graduated")
+getVoice("graduated from the school of badassery")
+getVoice("everything explodes in the background and an eagle screeches overhead")
+getVoice("neko-chan hums the star spangled banner")
+getVoice("neko-chan is a true american hero")
+getVoice("A KAWAIIDESUKA GAME")
+getVoice("WHY THE HELL DID YOU FINISH THIS")
+getVoice("SERIOUSLY WHAT IS WRONG WITH YOU")
